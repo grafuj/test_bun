c7d732146bb5c0cef6db804b82958099531f7727
grafuj <sfrrki@gmail.com>
2026-02-19 21:24:58 -0800

    add figlet, index.html, served it as default

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
+import figlet from "figlet";
+import index from "./index.html";
+
+// Initial console return
 // console.log("Hello via Bun!");
 
+//server to visit
 const server = Bun.serve({
   port: 3000,
   routes: {
-    "/": () => new Response("Bun!")
+    "/": index,
+    "/figlet": () => {
+      const body = figlet.textSync("Oh no, here I go again!");
+      return new Response(body);
+    }
   }
 });
 
--- notes.md
+++ notes.md
@@ -1,3 +1,4 @@
 # Day 1
 bun seems like a great efficient solution to some of node's issues
 
+without a frontend framework its a bit barebones, but lets see how it goes. Server seems pleasantly easy.
